a6a758e13e4c4a61fb8449c0c3c9b87eca03f1aa
kdk4905 <kdk4905@gmail.com>
2023-05-07 16:48:33 +0900

    C# 중급강의 > 라이브러리 만들기 > BabyCarrot

---
+++ BabyCarrot/Tools/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyCarrot.Tools
+{
+    //프로젝트의 폴더 위치를 반환하는 클래스
+    public static class Application
+    {
+        public static string Root 
+        {
+            get 
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+    }
+}
---
+++ BabyCarrotTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BabyCarrot.Tools;
+
+namespace BabyCarrotTest
+{
+    /// <summary>
+    /// BabyCarrot 라이브러리를
+    /// 테스트 하는 프로젝트
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 사용방법 1
+            // using BabyCarrot.Tools;
+            // 정적메소드 Application.Root 호출
+
+            // 사용방법 2
+            // Console.WriteLine(BabyCarrot.Tools.Application.Root);
+            Console.WriteLine(Application.Root);
+            Console.ReadLine();
+        }
+    }
+}
